792cf64fec7abab616bd3df63024eaec82b2e51a
jyop1212hy <jyop1212@icloud.com>
2025-10-17 00:36:23 +0900

    CH2 계산기 과제 계산기 프로그램 코딩 1차 추가 수정 및 보안 (100% 완성)

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -12,46 +12,62 @@ public class Calculator {
         //Scanner를 사용하여 양의 정수 2개(0 포함)와 사칙연산을 입력 받기
         Scanner calculatorInPut = new Scanner(System.in);
 
-        System.out.print("원하는 사칙연산(+,-,*,/) 기호를 입력하세요: ");
-        char operation = calculatorInPut.next().charAt(0);
-        //if문으로 사칙연산을 잘못 입력시 안내멘트 출력하고 프로그램 종료하기
-        if (operation != '+' && operation != '-' && operation != '*' && operation != '/') {
-            System.out.print("잘못된 연산을 입력하셨습니다.");
-            return;
+        // 7반복문 으로 무산 반복
+        while (true) {
+            // 1. 사칙연산 기로 입력 받기
+            System.out.print("원하는 사칙연산(+,-,*,/) 기호를 입력하세요: ");
+            char operation = calculatorInPut.next().charAt(0);
+
+            // 6. if문으로 사칙연산을 잘못 입력시 안내멘트 출력하고 재입력 받기
+            if (operation != '+' && operation != '-' && operation != '*' && operation != '/') {
+                System.out.print("연산기호를 다시 입력하세요.");
+                System.out.println("");
+                continue;
+            }
+
+            // 2. 첫번째 숫자 입력
+            System.out.print("첫번째 숫자를 입력하세요: ");
+            int num1 = calculatorInPut.nextInt();
+
+            // 3. 두번째 숫자 입력
+            System.out.print("두번째 숫자를 입력하세요: ");
+            int num2 = calculatorInPut.nextInt();
+
+            // 요청사항: “나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.“
+            // 5. 나눗셈 이기에 두번째 입력 정수는 0을 입력 할수 없게 한다.
+            if (operation == '/' && num2 == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)애 0이 입력될 수 없습니다.");
+                continue;
+            }
+            // 4. switch-case문으로 결과값 출력
+            int result = 0;
+            switch (operation) {
+
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+            System.out.println("결과: " + result);
+
+            // 8. 반복의 종료를 알려주는 “exit” 문자열을 입력하기
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            calculatorInPut.nextLine();
+            String exit = calculatorInPut.nextLine();
+            if (exit.equals("exit")) {
+                break;
+            }
         }
-
-        System.out.print("첫번째 숫자를 입력하세요: ");
-        int num1 =  calculatorInPut.nextInt();
-
-        System.out.print("두번째 숫자를 입력하세요: ");
-        int num2 = calculatorInPut.nextInt();
-
-        //나눗셈 이기에 두번째 입력 정수는 0을 입력 할수 없게 한다.
-        if(operation == '/' && num2 == 0){
-            System.out.println("나눗셈을 할떄 두번째 정수는 0을 입력할 수 없습니다.");
-            return;
-        }
-        // switch-case문으로 결과값 출력
-        // “나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.“
-        int result = 0;
-        switch (operation){
-
-            case '+':
-                result =  num1 + num2;
-                break;
-
-            case '-':
-                result =  num1 - num2;
-                break;
-
-            case '*':
-                result =  num1 * num2;
-                break;
-
-            case '/':
-                result =  num1 / num2;
-                break;
-        }
-        System.out.println("결과: " + result);
     }
 }
