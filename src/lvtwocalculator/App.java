bdc0a9e5377c44e36cda964a3b71c2d0d97b2465
jyop1212hy <jyop1212@icloud.com>
2025-10-21 03:01:07 +0900

    CH2. LV2계산기 과제 리펙터링 1) 코드 주석 정리 2) 프로그램 종료 기능 버그 수정 3) 계산기 추가 기능 이미지화 4) 계산기 추가 기능 - 순서변경 5) println에 \n 추가로 실행창 가독성 향상 6) Calculate 클래스- return num2; 삭제 7) Calculate 클래스- else if (indexNumber != 0) -> else 로 변경 8) public int result; 삭제 9) this.result = result; 삭제

--- src/lvtwocalculator/App.java
+++ src/lvtwocalculator/App.java
@@ -5,102 +5,112 @@ package lvtwocalculator;
 
     public class App {
         public static void main(String[] args) {
-            // Lv 1. 클래스 없이 기본적인 연산을 수행할 수 있는 계산기
-            //1) 계산기는 2개의 숫자를 받을 수 있고 사칙연산 될 문자를 받을 수 있다.
-            //2) 계산기는 exit을 입력할 때까지 계속해서 값을 받고 연산 결과를 반환한다.
-
             // Lv 2. 클래스를 적용해 기본적인 연산을 수행할 수 있는 계산기 만들기
             //사칙연산을 수행 후, 결과값 반환 메서드 구현 & 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
             //사칙연산을 수행한 후, 결과값을 반환하는 메서드 구현
             //연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
-            // 1) 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(➕,➖,✖️,➗) 기능을 수행한 후
-            // 2) 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성합니다.
+            // 1) 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(➕,➖,✖️,➗) 기능을 수행하기
+            // 2) 결과 값을 반환하는 메서드와 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성하기
+            // 6) 소스 코드 수정 후에도 수정 전의 기능들이 반드시 똑같이 동작해야합니다.
 
-
-            // 10. Calculator 인스턴스(객체) 생성
+            //Calculator 인스턴스(객체) 생성
             Calculator calculator = new Calculator();
 
             //Scanner를 사용하여 양의 정수 2개(0 포함)와 사칙연산을 입력 받기
-            // 11. calculatorInPut 변수를 Calculator 연산기능 메서드 매게변수 인자로 값을 넘기기
+            //calculatorInPut 변수를 Calculator 연산기능 메서드 매게변수 인자로 값을 넘기기
             Scanner calculatorInPut = new Scanner(System.in);
 
-            // 7반복문 으로 무산 반복
+            //반복문 으로 무산 반복
+            System.out.println("Hellow Calculator! 2.5.3 버전을 시작합니다.");
             while (true) {
-                // 1. 사칙연산 기로 입력 받기
-                System.out.print("\n원하는 계산(+,-,*,/) 기호를 입력하세요: ");
-                String operation = calculatorInPut.nextLine();
+                //사칙연산 기로 입력 받기
+                System.out.print("원하는 계산(+,-,*,/) 기호를 입력하세요: ");
+                String operation = calculatorInPut.next();
 
-                // 6. if문으로 사칙연산을 잘못 입력시 안내멘트 출력하고 재입력 받기
                 char operation1 = operation.charAt(0);
                 if (operation1 != '+' && operation1 != '-' && operation1 != '*' && operation1 != '/') {
                     System.out.print("계산기호를 다시 입력하세요.");
                     continue;
                 }
 
-                // 2. 첫번째 숫자 입력
+                //첫번째 숫자 입력
                 System.out.print("첫번째 숫자를 입력하세요: ");
                 int num1 = calculatorInPut.nextInt();
 
-                // 3. 두번째 숫자 입력
+                //두번째 숫자 입력
                 System.out.print("두번째 숫자를 입력하세요: ");
                 int num2 = calculatorInPut.nextInt();
+                calculatorInPut.nextLine(); // ← 숫자 읽은 뒤 개행 정리
 
-                // 19. 입력 받은 데이터를 매게변수를 통해 clculator 연산메서드로 인자값 전달
-                calculator.calculate(operation1, num1, num2);
-
-
-                // 20. 간접 접근을 통해 필드에 접근하여 가져올 수 있도록 구현합니다. (Getter 메서드)
-                int result = calculator.result;
-                System.out.println("결과: " + result);
+                //입력 받은 데이터를 매게변수를 통해 clculator 연산메서드로 인자값 전달
+                int result = calculator.calculate(operation1, num1, num2);
+                System.out.println("결과: " + result + "\n");
 
 
-                List<String> resultHistory = calculator.getResultHistory();
-                System.out.println("\n계산 기록");
-
-                for (int i = 0; i < resultHistory.size(); i++) {
-                    System.out.println(i + ". " + resultHistory.get(i));// ← 개별 항목 출력
-                }
-
-
-                System.out.print("\n계산기 추가 기능 (원하는 기능의 번호를 입력하세요)\n");
-                System.out.print("1. 수정\n2. 삭제\n3. 추가 계산\n4. 프로그램 종료\n번호 입력 : ");
+                //getter,setter,오래된기록삭제 메뉴목록 형식 switchfh로 구현
+                System.out.print("""
+                                -----계산기 추가 기능 (원하는 기능의 번호를 입력하세요)----
+                                |  1. 추가 계산                                  |
+                                |  2. 기록 조회 및 수정                            |
+                                |  3. 기록 삭제 (가장 오래된 기록 삭제)               |
+                                |  4. 프로그램 종료                               |
+                                ------------------------------------------------
+                                번호 입력: """);
                 int menu = calculatorInPut.nextInt();
+                calculatorInPut.nextLine(); // ← menu 번호 읽은 뒤 개행 정리
+
+                // 7-1) getter를 private를 private된 resultHistory기록 인스턴화
+                List<String> resultHistory = calculator.getResultHistory();
 
                 switch (menu) {
-                    case 1:
-                        System.out.print("수정 하실 번호를 입력해주세요: ");
+                    case 1: {
+                        System.out.println("\n계산을 계속합니다.\n");
+                        break;
+                    }
+
+                // 7-2) getter로 resultHistory기록 보기
+                    case 2: {
+                        System.out.println("\n------계산 기록------");
+                        for (int i = 0; i < resultHistory.size(); i++) {
+                            System.out.println("|   " + i + ". " + resultHistory.get(i) + "  |");// ← 개별 항목 출력
+                            System.out.println("-------------------");
+                        }
+
+                    // 8-1) setter로 저장된 resultHistory기록 수정하기
+                        System.out.print("\n수정 하실 번호를 입력해주세요: ");
                         int indexNumber = calculatorInPut.nextInt();
                         calculatorInPut.nextLine();
-                        // 21. 간접 접근을 통해 필드에 접근하여 수정할 수 있도록 구현합니다. (Setter 메서드)
+
                         System.out.println("양식에 맞춰 수정하세요 (예: 2 + 2 = 4): ");
                         String changedData = calculatorInPut.nextLine();
                         calculator.setResultHistoryChangedData(indexNumber, changedData);
                         break;
+                    }
 
-                    case 2:
-                        //저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능
-                        System.out.print("숫자 0을 눌러 오래된 데이터순서로 삭제 할수 있습니다.");
+                    // 9-1) main 메서드에 삭제 메서드가 활용될 수 있도록
+                    //저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능
+                    case 3: {
+                        System.out.print("\n숫자 0을 눌러 오래된 데이터순서로 삭제 할수 있습니다.");
+                        System.out.println("\n------계산 기록------");
+                        for (int i = 0; i < resultHistory.size(); i++) {
+                            System.out.println("|   " + i + ". " + resultHistory.get(i) + "  |");// ← 개별 항목 출력
+                            System.out.println("-------------------" + "\n");
+                        }
                         int deleteNumber = calculatorInPut.nextInt();
-                       //calculatorInPut.nextLine();
+                        calculatorInPut.nextLine(); // 개행정리
                         calculator.resultHistoryRemoveDate(deleteNumber);
                         break;
+                    }
 
-                    case 3:
-                        System.out.println("계산을 계속합니다.");
-                        calculatorInPut.nextLine(); // 버퍼 정리
-                        break;
-
-                    case 4:
-                        // 8. 반복의 종료를 알려주는 “exit” 문자열을 입력하기
-                        System.out.println("'exit' 입력 하여 종료합니다");
-                        calculatorInPut.nextLine();
-                        String exit2 = calculatorInPut.nextLine();
-                        if (exit2.equals("exit")) {
+                    // 10)반복의 종료를 알려주는 “exit” 문자열을 입력하기
+                    case 4: {
+                        System.out.println("\n'exit' 입력 하여 종료합니다");
+                        if (calculatorInPut.nextLine().equals("exit")) {
                             System.out.println("프로그램을 종료 합니다.");
-                            break;
+                            return;
                         }
-
-                    System.out.println("오케이 렛츠기릿!");
+                    }
+                    default: System.out.println("메뉴 번호를 다시 입력해주세요.");
                 }
             }
         }
--- src/lvtwocalculator/Calculator.java
+++ src/lvtwocalculator/Calculator.java
@@ -4,20 +4,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+// 3) Calculator 클래스를 생성
 public class Calculator {
 
-    //속
-    //13. 연산 결과는 Calculator 클래스의 연산 결과를 저장하는 필드에 저장
-    //13-1 컬렉션 필드에 직접 접근하지 못하도록 수정 (캡슐화)
+    //속성
+    //연산 결과는 Calculator 클래스의 연산 결과를 저장하는 필드에 저장
+    //Main 에서컬렉션 필드에 직접 접근하지 못하도록 수정 (캡슐화)
     private List<String> resultHistory = new ArrayList<>();
-    public int result;
-    //생
 
-    //기
+    //생성자
 
-    //9. 연산 수행 역할은 Calculator 클래스가 담당
-    //9-1 연산기능 (char operation , int num1, int num2)매개변수 생성
-    //12. char operation -> char calculatorInPut 로 매개변수 변경
+    //기능
+
+    // 4) 연산 수행 역할은 Calculator 클래스가 담당
+    //연산기능 (char operation , int num1, int num2)매개변수 생성
+    //char operation -> char calculatorInPut 로 매개변수 변경
     public int calculate(char operation1, int num1, int num2) {
         int result = 0;
 
@@ -32,33 +33,30 @@ public class Calculator {
                 result = num1 * num2;
                 break;
             case '/':
-                // 요청사항: “나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.“
-                //15. 나눗셈 0입력 if문 병합 완료후 /(나눗셈 기호) 부분 제거)
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)애 0이 입력될 수 없습니다.");
-                    return num2;
+                //요청사항: “나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.“
+                //나눗셈 0입력 if문 병합 완료후 /(나눗셈 기호) 부분 제거)
+            if (num2 == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)애 0이 입력될 수 없습니다.");
                 } else {
-                    result = num1 / num2;
-                    break;
-                }
+                result = num1 / num2;
+                break;
+            }
         }
-        this.result = result;
-        //결과값 반환
 
-
-        // 16.결과값 기록 저장(저장이 정상 작동 되었는지 출력문 띄우기)
+        // 5) 연산 결과는 Calculator 클래스의 연산 결과를 저장하는 필드에 저장
+        //결과값 기록 저장(저장이 정상 작동 되었는지 출력문 띄우기)
         String save = num1 + " " + operation1 + " " + num2 + " = " + result;
         resultHistory.add(save);
         System.out.println("연산이 계산기에 저장되었습니다.");
         return result;
     }
 
-    // 17. 게터를 통해 Mian클래스에서 데이터 볼수있게 함
+    // 7) 간접 접근을 통해 필드에 접근하여 가져올수 있도록 구현함 (Getter 메서드)
     public List<String> getResultHistory() {
         return Collections.unmodifiableList(resultHistory);
     }
 
-    // 17-1. 간접 접근을 통해 필드에 접근하여 수정할 수 있도록 구현합니다. (Setter 메서드)
+    // 8) 간접 접근을 통해 필드에 접근하여 수정할 수 있도록 구현함 (Setter 메서드)
     public void setResultHistoryChangedData(int indexNumber, String changedData) {
         if (indexNumber < 0 || indexNumber >= resultHistory.size()) {
             System.out.println("해당번호는 없는 번호입니다.");
@@ -69,24 +67,15 @@ public class Calculator {
         return;
     }
 
-    // 17-1. 삭제
+    // 9) 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드를 구현
+    //오래된 기능 삭제 메서드
     public void resultHistoryRemoveDate(int indexNumber) {
         if (indexNumber == 0) {
             resultHistory.remove(indexNumber);
-            System.out.println("가장 오래된 기록이 삭제되었습니다!");
+            System.out.println("가장 오래된 기록이 삭제되었습니다!\n");
         } else {
-            System.out.println("삭제할 기록이 없습니다.");
+            System.out.println("해당 버전에서는 오래된 순서의 데이터만 삭제할수 있습니다.");
             return;
         }
     }
-
-
-    //    public boolean removeLast() {
-//        if (!resultHistory.isEmpty()) {
-//            resultHistory.remove(resultHistory.size() - 1);
-//            return true;
-//        }
-//        return false;
-//    }
 }
-
